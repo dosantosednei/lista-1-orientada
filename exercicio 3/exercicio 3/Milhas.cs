5e7d0b529ee907ee1ea86bccfc2a4981ffca8267
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-09-01 19:55:49 -0300

    Add files via upload

---
+++ exercicio 3/exercicio 3/Milhas.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_3
+{
+    internal class Quadrado2
+    {
+        #region Atributos
+        int d;
+        
+        int area;
+        #endregion
+
+        #region Construtores
+        public Quadrado2()
+        {
+            this.d = 0;
+          
+        }
+
+        public Quadrado2(int d )
+        {
+            this.d = d;
+            
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void setDiagonal(int d)
+        {
+            this.d = d;
+        }
+
+        
+        
+            
+        
+
+        public int getDiagonal()
+        {
+            return this.d;
+        }
+
+        
+        
+           
+        
+
+        public int getresultado()
+        {
+            return this.area;
+        }
+        #endregion
+
+        #region Métodos funcionais
+        public void CalcularResultado()
+        {
+            this.area = (this.d*this.d) / 2;
+        }
+        #endregion
+    }
+}
---
+++ exercicio 3/exercicio 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado2 quadrado2 = new Quadrado2( 8);
+            quadrado2.CalcularResultado();
+            Console.WriteLine(quadrado2.getresultado());
+        }
+    }
+}
