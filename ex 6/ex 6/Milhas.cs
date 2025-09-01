c1cb06a0e228c79de6928c06cc3e4a05a47d119a
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-09-01 20:18:47 -0300

    Add files via upload

---
+++ ex 6/ex 6/Milhas.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_6
+{
+    internal class Milhas
+    {
+        #region Atributos
+        double milha;
+        
+        double km;
+        #endregion
+
+        #region Construtores
+        public Milhas()
+        {
+            this.km = 0;
+            this.milha = 0;
+        }
+
+        public Milhas(double milha)
+        {
+            
+            this.milha = milha;
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void setMilha(double milha)
+        {
+            this.milha = milha;
+        }
+
+        
+        
+           
+        
+
+        public double getMilha()
+        {
+            return this.milha;
+        }
+
+        
+        
+           
+        
+
+        public double getresultado()
+        {
+            return this.km;
+        }
+        #endregion
+
+        #region Métodos funcionais
+        public void CalcularResultado()
+        {
+            this.km = this.milha * 1.852;
+        }
+        #endregion
+    }
+}
---
+++ ex 6/ex 6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Milhas milhas = new Milhas(6);
+            milhas.CalcularResultado();
+            Console.WriteLine(milhas.getresultado());
+        }
+    }
+}
