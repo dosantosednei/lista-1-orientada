97a581380339adb2d074f15aa06a6590c53331df
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-09-01 20:27:04 -0300

    Add files via upload

---
+++ ex 7/ex 7/Dolar.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_7
+{
+    internal class Dolar
+    {
+        #region Atributos
+        double dolar;
+
+        double real;
+        #endregion
+
+        #region Construtores
+        public Dolar()
+        {
+            this.real = 0;
+            this.dolar = 0;
+        }
+
+        public Dolar(double dolar)
+        {
+
+            this.dolar = dolar;
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void setDolar(double dolar)
+        {
+            this.dolar = dolar;
+        }
+
+
+
+
+
+
+        public double getDolar()
+        {
+            return this.dolar;
+        }
+
+
+
+
+
+
+        public double getresultado()
+        {
+            return this.real;
+        }
+        #endregion
+
+        #region Métodos funcionais
+        public void CalcularResultado()
+        {
+            this.real = this.dolar * 5.45;
+        }
+        #endregion
+    }
+}
---
+++ ex 7/ex 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar dolar = new Dolar(10);
+            dolar.CalcularResultado();
+            Console.WriteLine(dolar.getresultado());
+        }
+    }
+}
