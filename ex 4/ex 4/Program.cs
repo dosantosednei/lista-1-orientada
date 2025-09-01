1a80363f9aa954d6eac06a2b32d988f781e09987
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-09-01 20:01:13 -0300

    Add files via upload

---
+++ ex 4/ex 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo(8,9);
+            triangulo.CalcularResultado();
+            Console.WriteLine(triangulo.getresultado());
+        }
+    }
+}
---
+++ ex 4/ex 4/Triangulo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_4
+{
+    internal class Triangulo
+    {
+        #region Atributos
+        int a;
+        int b;
+        int area;
+        #endregion
+
+        #region Construtores
+        public Triangulo()
+        {
+            this.a = 0;
+            this.b = 0;
+        }
+
+        public Triangulo(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void setLadoa(int a)
+        {
+            this.a = a;
+        }
+
+        public void setLadob(int b)
+        {
+            this.b = b;
+        }
+
+        public int getLadoa()
+        {
+            return this.a;
+        }
+
+        public int getLadob()
+        {
+            return this.b;
+        }
+
+        public int getresultado()
+        {
+            return this.area;
+        }
+        #endregion
+
+        #region Métodos funcionais
+        public void CalcularResultado()
+        {
+            this.area = (this.a * this.b) / 2;
+        }
+        #endregion
+    }
+}
