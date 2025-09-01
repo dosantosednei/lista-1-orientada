1b6c4e7fc9e9ca51ce20cc6d87c05ff4607a1598
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-09-01 19:41:38 -0300

    Add files via upload

---
+++ pooex2/pooex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado = new Quadrado(9,9);
+            quadrado.CalcularResultado();
+            Console.WriteLine(quadrado.getresultado());
+        }
+    }
+}
---
+++ pooex2/pooex2/Quadrado.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooex2
+{
+    internal class Quadrado
+    {
+        #region Atributos
+        int a;
+        int b;
+        int area;
+        #endregion
+
+        #region Construtores
+        public Quadrado()
+        {
+            this.a = 0;
+            this.b = 0;
+        }
+
+        public Quadrado(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void setLadoa(int a)
+        {
+            this.a = a;
+        }
+
+        public void setLadob(int b)
+        {
+            this.b = b;
+        }
+
+        public int getLadoa()
+        {
+            return this.a;
+        }
+
+        public int getLadob()
+        {
+            return this.b;
+        }
+
+        public int getresultado()
+        {
+            return this.area;
+        }
+        #endregion
+
+        #region Métodos funcionais
+        public void CalcularResultado()
+        {
+            this.area = this.a * this.b;
+        }
+        #endregion
+    }
+}
