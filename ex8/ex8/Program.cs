b81a443b7f8107470188776361414a9524a841ab
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-09-01 20:41:56 -0300

    Add files via upload

---
+++ ex8/ex8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior maior = new Maior(9,9);
+            maior.CalcularResultado();
+            Console.WriteLine(maior.getMaior());
+        }
+    }
+}
