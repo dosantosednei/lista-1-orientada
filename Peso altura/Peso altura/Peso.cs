34e370a48eab84b2e781994d739cfa33935a65ca
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-09-08 19:50:40 -0300

    Add files via upload

---
+++ Peso altura/Peso altura/Peso.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peso_altura
+{
+    internal class Peso
+    {
+        #region Atributos
+        double a;
+        double b;
+        double r;
+        #endregion
+
+        #region Construtores
+        public Peso()
+        {
+            this.a = 0;
+            this.b = 0;
+            this.r = 0;
+        }
+
+        public Peso(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void setAltura(double a)
+        {
+            this.a = a;
+        }
+
+        public void setPeso(double b)
+        {
+            this.b = b;
+        }
+
+        public double getAltura()
+        {
+            return this.a;
+        }
+
+        public double getPeso()
+        {
+            return this.b;
+        }
+
+        public double getRelacao()
+        {
+            return this.r;
+        }
+        #endregion
+
+         
+        public void CalcularRelacao()
+        {
+            this.r = this.b / (this.a * this.a);
+            if(this.r < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (this.r >= 25)
+            {
+                Console.WriteLine("Acima do peso");
+            }
+            if (this.r >= 20 && this.r < 25)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+        }
+    }
+}
---
+++ Peso altura/Peso altura/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peso_altura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Peso p = new Peso();
+
+            Console.WriteLine("Digite o peso: ");
+            p.setPeso(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite a altura");
+            p.setAltura(double.Parse(Console.ReadLine()));
+            p.CalcularRelacao();
+            Console.WriteLine((p.getRelacao()));
+        }
+    }
+}
